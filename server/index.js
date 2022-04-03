5db69c7922b45cd5adc5b9ed2e2be7a92a63af7d
Yalegama <nadunyalegama@.com>
2022-04-03 18:05:07 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,35 +1,43 @@
-const express=require('express');
-const app=express();
-const bodyparse=require('body-parser');
-const cors=require('cors');
-const mysql=require('mysql');
-const PORT=process.env.PORT || 3001;
+const fs = require('fs');
+const mysql = require('mysql');
+const csv = require('fast-csv');
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyparse.urlencoded({extended:true}));
+// Import CSV Data to MySQL database
+importCsvData2MySQL('customers.csv');
 
-const db=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'hrdashboard'
-})
+function importCsvData2MySQL(filename){
+	let stream = fs.createReadStream(filename);
+	let csvData = [];
+	let csvStream = csv
+		.parse()
+		.on("data", function (data) {
+			csvData.push(data);
+		})
+		.on("end", function () {
+			// -> Remove Header ROW
+			csvData.shift();
+			
+			// -> Create a connection to the database
+			const connection = mysql.createConnection({
+				host: 'localhost',
+				user: 'root',
+				password: '',
+				database: 'hrdashboard'
+			});
 
+			// Open the MySQL connection
+			connection.connect((error) => {
+				if (error) {
+					console.error(error);
+				} else {
+					let query = 'INSERT INTO etoreport (date, area, internalcarder, actualcarder, vop, resign, total, voppercentage, eto, flowarea, shift, vsl, er, gl, godfather, lokuakka) VALUES ?';
+					connection.query(query, [csvData], (error, response) => {
+						console.log(error || response);
+                        
+					});
+				}
+			});
+		});
 
-app.get('/addeto',(req,res)=>{
-    const value=req.body.value;
-    const test=("INSERT INTO test(area) VALUES ('Team - 02 - A - SY')");
-    db.query(test,[value],(err,result)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.log(result)
-        }
-    })
-})
-
-
-app.listen(PORT,(req,res)=>{
-    console.log(`Server is started on ${PORT}`);
-});
+	stream.pipe(csvStream);
+}
